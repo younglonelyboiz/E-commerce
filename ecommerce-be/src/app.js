1ea5f1500cb603a208706bfa3635fbb2d1b2b2b0
Hoang Ngoc Duc <duchbvts@gmail.com>
2026-04-24 13:30:36 +0700

    ahiah

--- ecommerce-be/src/app.js
+++ ecommerce-be/src/app.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://link-frontend-cua-duc.onrender.com",
+    ],
     credentials: true,
   }),
 );
