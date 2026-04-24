7ff51234619467e454f4d9fe30455b9cb69f1d75
Hoang Ngoc Duc <duchbvts@gmail.com>
2026-04-24 13:34:16 +0700

    check

--- ecommerce-be/src/app.js
+++ ecommerce-be/src/app.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://link-frontend-cua-duc.onrender.com",
+      "https://e-commerce-fe-hmfd.onrender.com",
     ],
     credentials: true,
   }),
